6f6bc31f4cbfcee0a9317c597b959eb93569b04f
manjukalal <manjukalal1496@gmail.com>
2022-05-27 22:01:51 +0530

    assignment1

---
+++ cmpstring.java
@@ -0,0 +1,13 @@
+public class Demostring {
+
+    public static void main(String[] args) {
+        String input1 = "abcdef";
+
+        String input2 = "abcdef";
+
+        if(input1 == input2)
+            System.out.println("Equal");
+        else
+            System.out.println("Not Equal");
+    }
+}
